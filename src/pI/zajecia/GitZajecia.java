2dde3fd869d029fdd4d73002b6f52784bd3f051f
Kamil Surlas <ks303231@student.polsl.pl>
2021-12-21 12:47:10 +0100

    zmiana w sklonowanym repozytorium

--- src/pI/zajecia/GitZajecia.java
+++ src/pI/zajecia/GitZajecia.java
@@ -4,5 +4,6 @@ public class GitZajecia {
     public static void main(String[] args) {
         System.out.println("Umieśc mnie na GitHubi'e");
         System.out.println("Zmiana w pliku");
+        System.out.println("Zmiana w pliku sklonowanym");
     }
 }
